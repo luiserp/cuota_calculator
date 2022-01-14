24b56717ce673219e5b6a33f664bd3c18c447ac8
luiserp <luisreyesperez98@gmail.com>
2022-01-14 20:03:37 +0100

    gh-pages commit

---
+++ precache-manifest.3376fe1669b05f25166b415858f90d2e.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3eb6435bbeecdc3c92f5",
+    "url": "/cuota_calculator/css/chunk-vendors.5d3035bc.css"
+  },
+  {
+    "revision": "e435cbe72f5271b077368b23ed4a342d",
+    "url": "/cuota_calculator/index.html"
+  },
+  {
+    "revision": "2c314de0b075c98e71c5",
+    "url": "/cuota_calculator/js/app.0c167a2d.js"
+  },
+  {
+    "revision": "c01d7b5fb0a8ed99eb57",
+    "url": "/cuota_calculator/js/chunk-20314126.44be4194.js"
+  },
+  {
+    "revision": "407bbf5c642bf127b7b8",
+    "url": "/cuota_calculator/js/chunk-2d230bc9.ea420351.js"
+  },
+  {
+    "revision": "5d2b52af4162ad2d5850",
+    "url": "/cuota_calculator/js/chunk-dec2f66e.e827dc2c.js"
+  },
+  {
+    "revision": "3eb6435bbeecdc3c92f5",
+    "url": "/cuota_calculator/js/chunk-vendors.23b4ef5b.js"
+  },
+  {
+    "revision": "d760802630e614fc481195e647cad03c",
+    "url": "/cuota_calculator/manifest.json"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/cuota_calculator/robots.txt"
+  }
+]);
